ed9367a17c989d1add269a14f925926cbe2220db
braggs <emielvdbrink@gmail.com>
2023-07-27 22:26:55 +0200

    added texturing

--- Consolica/Consolica.cpp
+++ Consolica/Consolica.cpp
@@ -2,17 +2,29 @@
 #include <chrono>
 #include <vector>
 #include <algorithm>
+#include <string>
 
 #include "olcConsoleGameEngine.h"
 
 class OneLoneCoder_UltimateFPS : public olcConsoleGameEngine
 {
-public:
-    OneLoneCoder_UltimateFPS()
-    {
-        m_sAppName = L"Ultimate First Person Shooter!";
-    }
+private:
+    //int screenWidth = 120;
+    //int screenHeight = 40;
+    int mapHeight = 16;
+    int mapWidth = 16;
 
+    float playerX = 8.0f;
+    float playerY = 8.0f;
+    float playerA = 0.0f;
+    float FOV = 3.14159 / 4.0;
+    float maxDepth = 16.0f;
+    float walkSpeed = 5.0f;
+
+    std::wstring map;
+    olcSprite* spriteWall;
+
+protected:
     virtual bool OnUserCreate()
     {
         map += L"################";
@@ -31,6 +43,8 @@ public:
         map += L"#..............#";
         map += L"#..............#";
         map += L"################";
+
+        spriteWall = new olcSprite(L"../Sprites/fps_wall1.spr");
         return true;
     }
 
@@ -39,34 +53,59 @@ public:
         //player input
         if (m_keys[L'A'].bHeld)
         {
-            playerA -= walkSpeed * fElapsedTime;
+            playerA -= 1.0f * fElapsedTime;
         }
         if (m_keys[L'D'].bHeld)
         {
-            playerA += walkSpeed * fElapsedTime;
+            playerA += 1.0f * fElapsedTime;
         }
 
         if (m_keys[L'W'].bHeld)
         {
-            playerX += sinf(playerA) * 5.0f * fElapsedTime; //shouldn't these be the other way around?
-            playerY += cosf(playerA) * 5.0f * fElapsedTime;
+            playerX += sinf(playerA) * walkSpeed * fElapsedTime; //shouldn't these be the other way around?
+            playerY += cosf(playerA) * walkSpeed * fElapsedTime;
 
             if (map[(int)playerY * mapWidth + (int)playerX] == '#')
             {
-                playerX -= sinf(playerA) * 5.0f * fElapsedTime;
-                playerY -= cosf(playerA) * 5.0f * fElapsedTime;
+                playerX -= sinf(playerA) * walkSpeed * fElapsedTime;
+                playerY -= cosf(playerA) * walkSpeed * fElapsedTime;
             }
         }
 
         if (m_keys[L'S'].bHeld)
         {
-            playerX -= sinf(playerA) * 5.0f * fElapsedTime;
-            playerY -= cosf(playerA) * 5.0f * fElapsedTime;
+            playerX -= sinf(playerA) * walkSpeed * fElapsedTime;
+            playerY -= cosf(playerA) * walkSpeed * fElapsedTime;
 
             if (map[(int)playerY * mapWidth + (int)playerX] == '#')
             {
-                playerX += sinf(playerA) * 5.0f * fElapsedTime;
-                playerY += cosf(playerA) * 5.0f * fElapsedTime;
+                playerX += sinf(playerA) * walkSpeed * fElapsedTime;
+                playerY += cosf(playerA) * walkSpeed * fElapsedTime;
+            }
+        }
+
+        //strafing
+        if (m_keys[L'E'].bHeld)
+        {
+            playerX += cosf(playerA) * walkSpeed * fElapsedTime; //shouldn't these be the other way around?
+            playerY -= sinf(playerA) * walkSpeed * fElapsedTime;
+
+            if (map[(int)playerY * mapWidth + (int)playerX] == '#')
+            {
+                playerX -= cosf(playerA) * walkSpeed * fElapsedTime;
+                playerY += sinf(playerA) * walkSpeed * fElapsedTime;
+            }
+        }
+
+        if (m_keys[L'Q'].bHeld)
+        {
+            playerX -= cosf(playerA) * walkSpeed * fElapsedTime;
+            playerY += sinf(playerA) * walkSpeed * fElapsedTime;
+
+            if (map[(int)playerY * mapWidth + (int)playerX] == '#')
+            {
+                playerX += cosf(playerA) * walkSpeed * fElapsedTime;
+                playerY -= sinf(playerA) * walkSpeed * fElapsedTime;
             }
         }
         //...
@@ -86,10 +125,12 @@ public:
             float vectorX = std::sinf(rayAngle); //same here?
             float vectorY = std::cosf(rayAngle);
 
+            float sampleX = 0.0f; //sample for texture
+
             //"march" forward from ray origin untill we hit a wall
             while (!hitWall && distanceToWall < maxDepth)
             {
-                distanceToWall += 0.1f;
+                distanceToWall += 0.01f;
 
                 //"sample" a position by taking the player pos + the raycast vector
                 int tileX = (int)(playerX + vectorX * distanceToWall);
@@ -107,39 +148,64 @@ public:
                     {
                         hitWall = true;
 
-                        //tile boundary check
-                        std::vector<std::pair<float, float>> distanceDot;
+                        float blockMidX = (float)tileX + 0.5f;
+                        float blockMidY = (float)tileY + 0.5f;
 
-                        for (int tx = 0; tx < 2; tx++)
+                        //exact collision points
+                        float testPointX = playerX + vectorX * distanceToWall;
+                        float testPointY = playerY + vectorY * distanceToWall;
+
+                        float testAngle = atan2((testPointY - blockMidY), (testPointX - blockMidX)); //? needs some more research
+                        if (testAngle >= -3.14159f * 0.25f && testAngle < 3.14159f * 0.25f) //check which quadrant we landed in, depends which axis to use for sampling
                         {
-                            for (int ty = 0; ty < 2; ty++)
-                            {
-                                float vy = (float)tileY + ty - playerY;
-                                float vx = (float)tileX + tx - playerX;
-                                float magnitude = sqrt(vx * vx + vy * vy);
-                                // a_x * b_x + a_y + b_y, keep in mind we use unit vectors
-                                float dot = (vectorX * vx / magnitude) + (vectorY * vy / magnitude);
-                                distanceDot.push_back(std::make_pair(magnitude, dot));
-                            }
+                            sampleX = testPointY - (float)tileY;
+                        }
+                        if (testAngle >= 3.14159f * 0.25f && testAngle < 3.14159f * 0.75f)
+                        {
+                            sampleX = testPointX - (float)tileX;
+                        }
+                        if (testAngle < -3.14159f * 0.25f && testAngle >= -3.14159f * 0.75f)
+                        {
+                            sampleX = testPointX - (float)tileX;
+                        }
+                        if (testAngle >= 3.14159f * 0.75f || testAngle < -3.14159f * 0.75f)
+                        {
+                            sampleX = testPointY - (float)tileY;
                         }
 
-                        std::sort(distanceDot.begin(), distanceDot.end(), [](const std::pair<float, float>& left, const std::pair<float, float>& right) {return left.first < right.first; });
+                        ////tile boundary check
+                        //std::vector<std::pair<float, float>> distanceDot;
 
-                        //the dot product is already normalized so, cos(o) = a . b,
-                        //meaning to get the angle we simply take the arccos of the dot product
-                        float bound = 0.01f;
-                        if (acos(distanceDot.at(0).second) < bound)
-                        {
-                            boundary = true;
-                        }
-                        if (acos(distanceDot.at(1).second) < bound)
-                        {
-                            boundary = true;
-                        }
-                        //if (acos(distanceDot.at(2).second) < bound)
+                        //for (int tx = 0; tx < 2; tx++)
+                        //{
+                        //    for (int ty = 0; ty < 2; ty++)
+                        //    {
+                        //        float vy = (float)tileY + ty - playerY;
+                        //        float vx = (float)tileX + tx - playerX;
+                        //        float magnitude = sqrt(vx * vx + vy * vy);
+                        //        // a_x * b_x + a_y + b_y, keep in mind we use unit vectors
+                        //        float dot = (vectorX * vx / magnitude) + (vectorY * vy / magnitude);
+                        //        distanceDot.push_back(std::make_pair(magnitude, dot));
+                        //    }
+                        //}
+
+                        //std::sort(distanceDot.begin(), distanceDot.end(), [](const std::pair<float, float>& left, const std::pair<float, float>& right) {return left.first < right.first; });
+
+                        ////the dot product is already normalized so, cos(o) = a . b,
+                        ////meaning to get the angle we simply take the arccos of the dot product
+                        //float bound = 0.01f;
+                        //if (acos(distanceDot.at(0).second) < bound)
                         //{
                         //    boundary = true;
                         //}
+                        //if (acos(distanceDot.at(1).second) < bound)
+                        //{
+                        //    boundary = true;
+                        //}
+                        ////if (acos(distanceDot.at(2).second) < bound)
+                        ////{
+                        ////    boundary = true;
+                        ////}
                     }
                 }
             }
@@ -149,15 +215,15 @@ public:
             int ceiling = (float)(ScreenHeight() / 2.0) - ScreenHeight() / ((float)distanceToWall);
             int floor = ScreenHeight() - ceiling;
 
-            short shade = ' ';
+            //short shade = ' ';
 
-            if (distanceToWall <= maxDepth / 4.0f)      shade = 0x2588; //very close
-            else if (distanceToWall <= maxDepth / 3.0f) shade = 0x2593;
-            else if (distanceToWall <= maxDepth / 2.0f) shade = 0x2592;
-            else if (distanceToWall < maxDepth)         shade = 0x2591;
-            else                                        shade = ' ';    //too far away
+            //if (distanceToWall <= maxDepth / 4.0f)      shade = 0x2588; //very close
+            //else if (distanceToWall <= maxDepth / 3.0f) shade = 0x2593;
+            //else if (distanceToWall <= maxDepth / 2.0f) shade = 0x2592;
+            //else if (distanceToWall < maxDepth)         shade = 0x2591;
+            //else                                        shade = ' ';    //too far away
 
-            if (boundary)                               shade = ' ';
+            //if (boundary)                               shade = ' ';
 
             for (int y = 0; y < ScreenHeight(); y++)
             {
@@ -169,22 +235,33 @@ public:
                 }
                 else if (y >= ceiling && y <= floor) //wall
                 {
-                    //screen[y * ScreenWidth() + x] = shade;
-                    Draw(x, y, shade);
+                    if (distanceToWall < maxDepth)
+                    {
+                        float sampleY = ((float)y - ceiling) / ((float)floor - (float)ceiling);
+                        Draw(x, y, spriteWall->SampleGlyph(sampleX, sampleY), spriteWall->SampleColour(sampleX, sampleY));
+                        //screen[y * ScreenWidth() + x] = shade;
+                        //Draw(x, y, shade);
+                    }
+                    else
+                    {
+                        Draw(x, y, PIXEL_SOLID, 0);
+                    }
                 }
                 else //ground
                 {
-                    //floor shading (how does this work?)
-                    float floorDistance = 1.0f - (((float)y - ScreenHeight() / 2.0f) / ((float)ScreenHeight() / 2.0f));
+                    Draw(x, y, PIXEL_SOLID, FG_DARK_GREEN);
 
-                    if (floorDistance < 0.25f) shade = '#';
-                    else if (floorDistance < 0.5f)  shade = 'x';
-                    else if (floorDistance < 0.75f) shade = '.';
-                    else if (floorDistance < 0.9f)  shade = '-';
-                    else                            shade = ' ';
+                    ////floor shading (how does this work?)
+                    //float floorDistance = 1.0f - (((float)y - ScreenHeight() / 2.0f) / ((float)ScreenHeight() / 2.0f));
 
-                    //screen[y * ScreenWidth() + x] = shade;
-                    Draw(x, y, shade);
+                    //if (floorDistance < 0.25f) shade = '#';
+                    //else if (floorDistance < 0.5f)  shade = 'x';
+                    //else if (floorDistance < 0.75f) shade = '.';
+                    //else if (floorDistance < 0.9f)  shade = '-';
+                    //else                            shade = ' ';
+
+                    ////screen[y * ScreenWidth() + x] = shade;
+                    //Draw(x, y, shade);
                 }
             }
         }
@@ -210,26 +287,17 @@ public:
         return true;
     }
 
-private:
-    //int screenWidth = 120;
-    //int screenHeight = 40;
-    int mapHeight = 16;
-    int mapWidth = 16;
-
-    float playerX = 8.0f;
-    float playerY = 8.0f;
-    float playerA = 0.0f;
-    float FOV = 3.14159 / 4.0;
-    float maxDepth = 16.0f;
-    float walkSpeed = 2.0f;
-
-    std::wstring map;
+public:
+    OneLoneCoder_UltimateFPS()
+    {
+        m_sAppName = L"Ultimate First Person Shooter!";
+    }
 };
 
 int main()
 {
     OneLoneCoder_UltimateFPS game;
-    game.ConstructConsole(120, 80, 8, 8);
+    game.ConstructConsole(320, 240, 4, 4);
     game.Start();
 
     return 0;
